5e28d78310b791a41edc9b911e367043fd120584
jjoadevy <jjoaquinarlettaz@gmail.com>
2023-09-23 19:48:59 -0300

    feat: translate all into spanish

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,8 +4,8 @@ type responseAll = {
 	docs: Simpson[]
 }
 
-export const getSimpsons = async () => {
-    const response = await fetch('https://apisimpsons.fly.dev/api/personajes?limit=50');
+export const getSimpsons = async (limit:number = 57,page?:number) => {
+    const response = await fetch(`https://apisimpsons.fly.dev/api/personajes?limit=${limit}` + (page ? `&page=${page}` : ''));
     const {docs:data}:responseAll = await response.json();
     return data;
 }
@@ -17,6 +17,5 @@ type responseOne = {
 export const getSimpsonByName = async (name:string) => {
     const response = await fetch(`https://apisimpsons.fly.dev/api/personajes/find/${name}`);
     const {result:data}:responseOne = await response.json();
-    console.log(name,data)
     return data.find((simpson) => simpson.Nombre.trim() === name)
 }
